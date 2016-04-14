7378530557e8c6f743c6dc23c16f84bb8fee1aec
Kazato Sugimoto <uiureo@gmail.com>
2016-04-14 17:06:05 +0900

    Add type check to main

--- main.go
+++ main.go
@@ -10,8 +10,21 @@ import (
 )
 
 func main() {
-	data, _ := ioutil.ReadAll(os.Stdin)
-	src := string(data)
+	var src string
+
+	if len(os.Args) > 1 {
+		filename := os.Args[1]
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		src = string(data)
+	} else {
+		data, _ := ioutil.ReadAll(os.Stdin)
+		src = string(data)
+	}
+
 	statements, err := Parse(src)
 	if err != nil {
 		fmt.Println(err)
@@ -28,20 +41,29 @@ func main() {
 	env := &Env{}
 	errs := Analyze(statements, env)
 	if len(errs) > 0 {
-		for _, err := range errs {
-			switch e := err.(type) {
-			case SemanticError:
-				lineNumber, columnNumber := posToLineInfo(src, int(e.Pos))
-				err = fmt.Errorf("%d:%d: %v", lineNumber, columnNumber, e.Err)
+		Exit(src, errs)
+	}
 
-			default:
-			}
-
-			fmt.Fprintln(os.Stderr, err)
-		}
-
-		os.Exit(1)
+	err = CheckType(statements)
+	if err != nil {
+		Exit(src, []error{err})
 	}
 
 	pp.Println(statements, env)
+}
+
+func Exit(src string, errs []error) {
+	for _, err := range errs {
+		switch e := err.(type) {
+		case SemanticError:
+			lineNumber, columnNumber := posToLineInfo(src, int(e.Pos))
+			err = fmt.Errorf("%d:%d: %v", lineNumber, columnNumber, e.Err)
+
+		default:
+		}
+
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	os.Exit(1)
 }
