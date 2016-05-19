e79687191447aada8407d7efb26cc70dcd8ef991
Kazato Sugimoto <uiureo@gmail.com>
2016-05-19 14:07:27 +0900

    Add example test using spim

--- main_test.go
+++ main_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"io/ioutil"
+	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -29,4 +32,64 @@ func TestCompileExample(t *testing.T) {
       t.Error("expect code to be present")
     }
 	}
+}
+
+func TestSimulateExample(t *testing.T) {
+	examples := [](struct {
+		Filename string
+		Output string
+	}){
+		{"example/sum.sc", "120"},
+		{"example/bubble_sort.sc", "12345678"},
+		{"example/quick_sort.sc", "12345678"},
+	}
+
+	for _, example := range examples {
+		sourceFilename := example.Filename
+		filename := regexp.MustCompile("\\.sc$").ReplaceAllString(sourceFilename, ".s")
+
+		{
+			err := compileAndSave(sourceFilename)
+
+			if err != nil {
+				t.Error(err)
+				continue
+			}
+		}
+
+		byteOut, err := exec.Command("spim", "-file", filename).Output()
+
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		lines := strings.Split(string(byteOut), "\n")
+		output := lines[len(lines)-1]
+		expected := example.Output
+
+		if output != expected {
+			t.Errorf("expect `%v`'s output to be `%v`, got `%v`", filename, expected, output)
+		}
+	}
+}
+
+func compileAndSave(filename string) error {
+	src, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return err
+  }
+
+  code, errs := CompileSource(string(src), true)
+  for _, err := range errs {
+		return err
+  }
+
+	dest := regexp.MustCompile("\\.sc$").ReplaceAllString(filename, ".s")
+	err = ioutil.WriteFile(dest, []byte(code), 0777)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
